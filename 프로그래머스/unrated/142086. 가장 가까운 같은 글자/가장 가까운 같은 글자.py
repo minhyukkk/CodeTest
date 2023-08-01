4c863ea9ac488af5fb2181c9b7fc80146aa4c174
minhyukkk <108050855+minhyukkk@users.noreply.github.com>
2023-08-01 16:55:31 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3.08 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,12 +1,11 @@
 def solution(s):
     answer = []
-    s_index = dict()
+    s_index = {}
     
     for i in range(len(s)):
-        if s[i] not in s_index:
+        if s[i] not in s_index: # s_index의 키 값에서 확인
             answer.append(-1)
         else:
             answer.append(i-s_index[s[i]])
         s_index[s[i]] = i
-    
     return answer
