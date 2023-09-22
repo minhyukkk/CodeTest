e60d4593730c6fb508e9c8b1bb2a4ae03987a88b
minhyukkk <108050855+minhyukkk@users.noreply.github.com>
2023-09-22 14:24:06 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34168 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,35 +1,30 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
-import sys 
 sys.setrecursionlimit(10**8)
 
 n, m = map(int, input().split())
-
 graph = []
+visited = [[0] * m for _ in range(n)]
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
-for _ in range(n):
-    graph.append(list(map(int,input())))
+for i in range(n):
+    graph.append(list(map(int, input().rstrip())))
 
 def bfs(x,y):
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
-
     q = deque()
     q.append((x,y))
+    visited[x][y] = 1
 
     while q:
-        x, y = q.popleft()
-        
+        a,b = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
+            nx = dx[i] + a
+            ny = dy[i] + b
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and graph[nx][ny] == 1:
+                visited[nx][ny] += visited[a][b] + 1
                 q.append((nx,ny))
 
-    return graph[n-1][m-1]
-
-print(bfs(0,0))
+bfs(0,0)
+print(visited[n-1][m-1])
