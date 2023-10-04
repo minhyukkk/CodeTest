bfadfaac812b3b36b9903e8dc0cb319652987caf
minhyukkk <108050855+minhyukkk@users.noreply.github.com>
2023-10-04 21:00:38 +0900

    [Silver I] Title: IOIOI, Time: 352 ms, Memory: 33212 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,21 @@
+# 출처 https://black-hair.tistory.com/135
+# 생각 자체를 못함
+
+n = int(input())
+m = int(input())
+s = input()
+
+answer, i, count = 0, 0, 0
+
+while i < (m-1):
+    if s[i:i+3] == "IOI":
+        i += 2
+        count += 1
+        if count == n:
+            answer += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
+
+print(answer)
